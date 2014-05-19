3ddfd73a4780816b90173304fab7a8c389bbfc4e
Grant Hudgens <grant.hudgens@gmail.com>
2014-05-18 20:43:58 -0400

    First real configuration.

--- config/projects/puppet-library.rb
+++ config/projects/puppet-library.rb
@@ -1,20 +1,47 @@
-
+#
+# Copyright:: Copyright (c) 2014 Grant Hudgens.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 name 'puppet-library'
-maintainer 'CHANGE ME'
-homepage 'CHANGEME.com'
+maintainer 'drrb'
+homepage 'https://github.com/drrb/puppet-library'
 
+#Package attributes
 install_path    '/opt/puppet-library'
-build_version   Omnibus::BuildVersion.new.semver
-build_iteration 1
+build_version   '0.13.0'
+build_iteration 0
 
-# creates required build directories
+# Specify dependency versions to embed
+override :'ruby', version: "2.0.0-p451"
+override :'passenger-gem', version: "4.0.42"
+override :'nginx', version: "1.6.0"
+override :'puppet-library-gem', version: "#{build_version}"
+
+#Creates required build directories
 dependency 'preparation'
 
-# puppet-library dependencies/components
-# dependency 'somedep'
+# Puppet Library dependencies/components
+dependency 'puppet-library-gem'
+dependency 'passenger-gem'
+dependency 'nginx'
 
-# version manifest file
+#Puppet Library and its dependencies' configuration/misc files
+
+#Version manifest file
 dependency 'version-manifest'
 
+#Ignore git crap
 exclude '\.git*'
 exclude 'bundler\/git'
---
+++ config/software/nginx.rb
@@ -0,0 +1,73 @@
+#
+# Copyright:: Copyright (c) 2014 Grant Hudgens.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+name "nginx"
+default_version "1.6.0"
+
+dependency "curl"
+dependency "pcre"
+dependency "openssl"
+dependency "passenger-gem"
+dependency "addressable-gem"
+
+version "1.4.4" do
+  source md5: "5dfaba1cbeae9087f3949860a02caa9f"
+end
+
+version "1.4.7" do
+  source md5: "aee151d298dcbfeb88b3f7dd3e7a4d17"
+end
+
+version "1.6.0" do
+  source md5: "8efa354f1c3c2ccf434a50d3fbe82340"
+end
+
+source url: "http://nginx.org/download/nginx-#{version}.tar.gz"
+
+relative_path "nginx-#{version}"
+
+env = { "LD_RUN_PATH"     => "#{install_dir}/embedded/lib",
+        "PATH"            => "#{install_dir}/embedded/bin:#{ENV["PATH"]}",
+        "BUNDLE_BIN_PATH" => "#{install_dir}/embedded/bin/bundle",
+        "RUBYOPT"         => nil,
+        "BUNDLE_GEMFILE"  => nil,
+        "GEM_PATH"        => nil,
+        "GEM_HOME"        => nil}
+
+# Grab the ruby and passenger-gem components as we will be using them in the configure
+ruby_cmpt = project.library.components.find { |c| c.name == 'ruby' }
+pgem_cmpt = project.library.components.find { |c| c.name == 'passenger-gem' }
+
+build do
+  configure_command = ["./configure",
+                       "--prefix=#{install_dir}/embedded",
+                       "--conf-path=#{install_dir}/embedded/etc/nginx/nginx.conf",
+                       "--pid-path=/var/run/nginx.pid",
+                       "--error-log-path=/var/log/puppet/nginx/error.log",
+                       "--http-log-path=/var/log/puppet/nginx/access.log",
+                       "--with-http_ssl_module",
+                       "--with-http_stub_status_module",
+                       "--with-http_gzip_static_module",
+                       "--add-module=#{install_dir}/embedded/lib/ruby/gems/#{ruby_cmpt.version.split("-p")[0]}/gems/passenger-#{pgem_cmpt.version}/ext/nginx",
+                       "--with-ld-opt=-L#{install_dir}/embedded/lib",
+                       "--with-cc-opt=\"-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include\"",
+                       "--with-debug"]
+
+  command configure_command.join(" "), :env => env
+  
+  command "make", :env => env
+  command "make install", :env => env
+end
